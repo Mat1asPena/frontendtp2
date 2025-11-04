da841627e5fc04d8f97e99b16458847c106af9de
Matias Agustin Peña <148594557+Mat1asPena@users.noreply.github.com>
2025-11-03 23:20:08 -0300

    minor fixs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Publicaciones } from "./pages/publicaciones/publicaciones";
 import { Navbar } from './shared/components/navbar/navbar';
 import { Footer } from './shared/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Publicaciones, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
