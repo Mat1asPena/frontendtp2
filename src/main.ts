c9a124f74fb3e6aa9caf826a141fa5c7bef86b62
Matias Agustin Peña <148594557+Mat1asPena@users.noreply.github.com>
2025-11-17 23:01:29 -0300

    ssr disable

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { App as App } from './app/app';
-
+import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { routes } from './app/app.routes';
 
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
-    importProvidersFrom(FormsModule, ReactiveFormsModule)
+    importProvidersFrom(FormsModule, ReactiveFormsModule),
   ]
 });
